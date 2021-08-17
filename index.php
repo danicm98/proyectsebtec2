de0a4423789d24a3151b9346f52293b010e068fa
danicm98 <50908422+danicm98@users.noreply.github.com>
2021-08-17 02:26:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+</head>
+<body>
+   
+   
+   <?php
+		
+		require_once("controller/login_controller.php");
+   
+   ?>
+	
+</body>
+</html>
