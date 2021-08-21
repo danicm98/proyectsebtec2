ed060c2b2b94c9ebd126cb1b49299d69e7f45cf5
danicm98 <50908422+danicm98@users.noreply.github.com>
2021-08-21 23:00:47 +0200

    Add files via upload

---
+++ controller/admin_controller.php
@@ -0,0 +1,18 @@
+
+
+<?php
+	
+	
+
+
+
+	require_once("../model/admin_model.php");
+	
+	
+	
+	require_once("../view/admin_view.php")
+
+
+
+
+?>
---
+++ controller/colab_controller.php
@@ -0,0 +1,16 @@
+<?php
+	
+	
+
+
+
+	require_once("../model/colab_model.php");
+	
+	
+	
+	require_once("../view/colab_view.php")
+
+
+
+
+?>
